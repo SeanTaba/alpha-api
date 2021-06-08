d2965dedb87736b5f31925c01baa28656cbdbe9f
Nicholas Recino <nick.recino@icloud.com>
2021-06-08 13:05:26 -0400

    Implemented AuthController and added in excpetions to be compliant for potential issues connecting to database

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,5 +1,9 @@
 package com.revature.services;
 
+import com.revature.exceptions.AuthenticationException;
+import com.revature.exceptions.DataSourceException;
+import com.revature.exceptions.ResourceNotFoundException;
+import com.revature.models.User;
 import com.revature.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +17,18 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userDao){
         this.userRepo = userDao;
+    }
+
+    @Transactional(readOnly = true)
+    public User authenticate(String username, String password) throws AuthenticationException {
+
+        try {
+            return userRepo.findUserByUsernameAndPassword(username, password)
+                    .orElseThrow(AuthenticationException::new);
+        } catch (Exception e) {
+            if (e instanceof ResourceNotFoundException) throw e;
+            throw new DataSourceException(e);
+        }
+
     }
 }
